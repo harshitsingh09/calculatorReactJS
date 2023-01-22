d11362c3484990fe13e1c646e2c54674fad83ba6
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2023-01-22 11:48:29 +0530

    project paused

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
 import './styles.css'
 
+export const ACTIONS = {
+  ADD_DIGIT: 'addDigit',
+  CHOOSE_OPERATION: 'chooseOperation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'deleteDigit',
+  EVALUATE: 'evaluate'
+}
+function reducer(state, {type, payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ....state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`
+      }
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer,{})
+
+  dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit: 1}})
   return (
     <div className="calculatorGrid">
       <div className="output">
-        <div className='previousOperand'>123, 234 *</div>
-        <div className="currentOperand">123, 234</div>
+        <div className='previousOperand'>{previousOperand} {operation}</div>
+        <div className="currentOperand">{currentOperand}</div>
       </div>
       <button className='spanTwo'>AC</button>
       <button>DEL</button>
+      <button>/</button>
       <button>1</button>
       <button>2</button>
       <button>3</button>
